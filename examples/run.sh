46f5ad321941e86ceb3e9dc9bf1a99085be9bfd6
Richard Wiedenhöft <richard.wiedenhoeft@gmail.com>
2013-08-11 11:18:01 +0200

    run.sh: now library is automatically built

--- examples/run.sh
+++ examples/run.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+cd ../build
+make
+cd ../examples
 valac -X -L../build -X -I../build -X -lwebcon -o example ./example.vala ../build/webcon.vapi
 LD_LIBRARY_PATH="../build" ./example -c ./example.conf
